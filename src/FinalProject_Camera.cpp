8f3f7054bdfad32ab585c7f87d4b647ae4ecdd00
Hagar Usama <Hagar_Usama@outlook.com>
2021-03-24 16:53:24 +0200

    add mid-term part

--- src/FinalProject_Camera.cpp
+++ src/FinalProject_Camera.cpp
@@ -35,9 +35,9 @@ int main(int argc, const char *argv[])
     string imgPrefix = "KITTI/2011_09_26/image_02/data/000000"; // left camera, color
     string imgFileType = ".png";
     int imgStartIndex = 0; // first file index to load (assumes Lidar and camera names have identical naming convention)
-    int imgEndIndex = 18;   // last file index to load
-    int imgStepWidth = 1; 
-    int imgFillWidth = 4;  // no. of digits which make up the file index (e.g. img-0001.png)
+    int imgEndIndex = 18;  // last file index to load
+    int imgStepWidth = 1;
+    int imgFillWidth = 4; // no. of digits which make up the file index (e.g. img-0001.png)
 
     // object detection
     string yoloBasePath = dataPath + "dat/yolo/";
@@ -50,33 +50,66 @@ int main(int argc, const char *argv[])
     string lidarFileType = ".bin";
 
     // calibration data for camera and lidar
-    cv::Mat P_rect_00(3,4,cv::DataType<double>::type); // 3x4 projection matrix after rectification
-    cv::Mat R_rect_00(4,4,cv::DataType<double>::type); // 3x3 rectifying rotation to make image planes co-planar
-    cv::Mat RT(4,4,cv::DataType<double>::type); // rotation matrix and translation vector
-    
-    RT.at<double>(0,0) = 7.533745e-03; RT.at<double>(0,1) = -9.999714e-01; RT.at<double>(0,2) = -6.166020e-04; RT.at<double>(0,3) = -4.069766e-03;
-    RT.at<double>(1,0) = 1.480249e-02; RT.at<double>(1,1) = 7.280733e-04; RT.at<double>(1,2) = -9.998902e-01; RT.at<double>(1,3) = -7.631618e-02;
-    RT.at<double>(2,0) = 9.998621e-01; RT.at<double>(2,1) = 7.523790e-03; RT.at<double>(2,2) = 1.480755e-02; RT.at<double>(2,3) = -2.717806e-01;
-    RT.at<double>(3,0) = 0.0; RT.at<double>(3,1) = 0.0; RT.at<double>(3,2) = 0.0; RT.at<double>(3,3) = 1.0;
-    
-    R_rect_00.at<double>(0,0) = 9.999239e-01; R_rect_00.at<double>(0,1) = 9.837760e-03; R_rect_00.at<double>(0,2) = -7.445048e-03; R_rect_00.at<double>(0,3) = 0.0;
-    R_rect_00.at<double>(1,0) = -9.869795e-03; R_rect_00.at<double>(1,1) = 9.999421e-01; R_rect_00.at<double>(1,2) = -4.278459e-03; R_rect_00.at<double>(1,3) = 0.0;
-    R_rect_00.at<double>(2,0) = 7.402527e-03; R_rect_00.at<double>(2,1) = 4.351614e-03; R_rect_00.at<double>(2,2) = 9.999631e-01; R_rect_00.at<double>(2,3) = 0.0;
-    R_rect_00.at<double>(3,0) = 0; R_rect_00.at<double>(3,1) = 0; R_rect_00.at<double>(3,2) = 0; R_rect_00.at<double>(3,3) = 1;
-    
-    P_rect_00.at<double>(0,0) = 7.215377e+02; P_rect_00.at<double>(0,1) = 0.000000e+00; P_rect_00.at<double>(0,2) = 6.095593e+02; P_rect_00.at<double>(0,3) = 0.000000e+00;
-    P_rect_00.at<double>(1,0) = 0.000000e+00; P_rect_00.at<double>(1,1) = 7.215377e+02; P_rect_00.at<double>(1,2) = 1.728540e+02; P_rect_00.at<double>(1,3) = 0.000000e+00;
-    P_rect_00.at<double>(2,0) = 0.000000e+00; P_rect_00.at<double>(2,1) = 0.000000e+00; P_rect_00.at<double>(2,2) = 1.000000e+00; P_rect_00.at<double>(2,3) = 0.000000e+00;    
+    cv::Mat P_rect_00(3, 4, cv::DataType<double>::type); // 3x4 projection matrix after rectification
+    cv::Mat R_rect_00(4, 4, cv::DataType<double>::type); // 3x3 rectifying rotation to make image planes co-planar
+    cv::Mat RT(4, 4, cv::DataType<double>::type);        // rotation matrix and translation vector
+
+    RT.at<double>(0, 0) = 7.533745e-03;
+    RT.at<double>(0, 1) = -9.999714e-01;
+    RT.at<double>(0, 2) = -6.166020e-04;
+    RT.at<double>(0, 3) = -4.069766e-03;
+    RT.at<double>(1, 0) = 1.480249e-02;
+    RT.at<double>(1, 1) = 7.280733e-04;
+    RT.at<double>(1, 2) = -9.998902e-01;
+    RT.at<double>(1, 3) = -7.631618e-02;
+    RT.at<double>(2, 0) = 9.998621e-01;
+    RT.at<double>(2, 1) = 7.523790e-03;
+    RT.at<double>(2, 2) = 1.480755e-02;
+    RT.at<double>(2, 3) = -2.717806e-01;
+    RT.at<double>(3, 0) = 0.0;
+    RT.at<double>(3, 1) = 0.0;
+    RT.at<double>(3, 2) = 0.0;
+    RT.at<double>(3, 3) = 1.0;
+
+    R_rect_00.at<double>(0, 0) = 9.999239e-01;
+    R_rect_00.at<double>(0, 1) = 9.837760e-03;
+    R_rect_00.at<double>(0, 2) = -7.445048e-03;
+    R_rect_00.at<double>(0, 3) = 0.0;
+    R_rect_00.at<double>(1, 0) = -9.869795e-03;
+    R_rect_00.at<double>(1, 1) = 9.999421e-01;
+    R_rect_00.at<double>(1, 2) = -4.278459e-03;
+    R_rect_00.at<double>(1, 3) = 0.0;
+    R_rect_00.at<double>(2, 0) = 7.402527e-03;
+    R_rect_00.at<double>(2, 1) = 4.351614e-03;
+    R_rect_00.at<double>(2, 2) = 9.999631e-01;
+    R_rect_00.at<double>(2, 3) = 0.0;
+    R_rect_00.at<double>(3, 0) = 0;
+    R_rect_00.at<double>(3, 1) = 0;
+    R_rect_00.at<double>(3, 2) = 0;
+    R_rect_00.at<double>(3, 3) = 1;
+
+    P_rect_00.at<double>(0, 0) = 7.215377e+02;
+    P_rect_00.at<double>(0, 1) = 0.000000e+00;
+    P_rect_00.at<double>(0, 2) = 6.095593e+02;
+    P_rect_00.at<double>(0, 3) = 0.000000e+00;
+    P_rect_00.at<double>(1, 0) = 0.000000e+00;
+    P_rect_00.at<double>(1, 1) = 7.215377e+02;
+    P_rect_00.at<double>(1, 2) = 1.728540e+02;
+    P_rect_00.at<double>(1, 3) = 0.000000e+00;
+    P_rect_00.at<double>(2, 0) = 0.000000e+00;
+    P_rect_00.at<double>(2, 1) = 0.000000e+00;
+    P_rect_00.at<double>(2, 2) = 1.000000e+00;
+    P_rect_00.at<double>(2, 3) = 0.000000e+00;
 
     // misc
     double sensorFrameRate = 10.0 / imgStepWidth; // frames per second for Lidar and camera
-    int dataBufferSize = 2;       // no. of images which are held in memory (ring buffer) at the same time
-    vector<DataFrame> dataBuffer; // list of data frames which are held in memory at the same time
-    bool bVis = false;            // visualize results
+    int dataBufferSize = 2;                       // no. of images which are held in memory (ring buffer) at the same time
+    vector<DataFrame> dataBuffer;                 // list of data frames which are held in memory at the same time
+    bool bVis = true;                             // visualize results
 
     /* MAIN LOOP OVER ALL IMAGES */
 
-    for (size_t imgIndex = 0; imgIndex <= imgEndIndex - imgStartIndex; imgIndex+=imgStepWidth)
+    for (size_t imgIndex = 0; imgIndex <= imgEndIndex - imgStartIndex; imgIndex += imgStepWidth)
     {
         /* LOAD IMAGE INTO BUFFER */
 
@@ -85,7 +118,7 @@ int main(int argc, const char *argv[])
         imgNumber << setfill('0') << setw(imgFillWidth) << imgStartIndex + imgIndex;
         string imgFullFilename = imgBasePath + imgPrefix + imgNumber.str() + imgFileType;
 
-        // load image from file 
+        // load image from file
         cv::Mat img = cv::imread(imgFullFilename);
 
         // push image into data frame buffer
@@ -95,16 +128,14 @@ int main(int argc, const char *argv[])
 
         cout << "#1 : LOAD IMAGE INTO BUFFER done" << endl;
 
-
         /* DETECT & CLASSIFY OBJECTS */
 
         float confThreshold = 0.2;
-        float nmsThreshold = 0.4;        
+        float nmsThreshold = 0.4;
         detectObjects((dataBuffer.end() - 1)->cameraImg, (dataBuffer.end() - 1)->boundingBoxes, confThreshold, nmsThreshold,
                       yoloBasePath, yoloClassesFile, yoloModelConfiguration, yoloModelWeights, bVis);
 
         cout << "#2 : DETECT & CLASSIFY OBJECTS done" << endl;
-
 
         /* CROP LIDAR POINTS */
 
@@ -116,29 +147,27 @@ int main(int argc, const char *argv[])
         // remove Lidar points based on distance properties
         float minZ = -1.5, maxZ = -0.9, minX = 2.0, maxX = 20.0, maxY = 2.0, minR = 0.1; // focus on ego lane
         cropLidarPoints(lidarPoints, minX, maxX, maxY, minZ, maxZ, minR);
-    
+
         (dataBuffer.end() - 1)->lidarPoints = lidarPoints;
 
         cout << "#3 : CROP LIDAR POINTS done" << endl;
-
 
         /* CLUSTER LIDAR POINT CLOUD */
 
         // associate Lidar points with camera-based ROI
         float shrinkFactor = 0.10; // shrinks each bounding box by the given percentage to avoid 3D object merging at the edges of an ROI
-        clusterLidarWithROI((dataBuffer.end()-1)->boundingBoxes, (dataBuffer.end() - 1)->lidarPoints, shrinkFactor, P_rect_00, R_rect_00, RT);
+        clusterLidarWithROI((dataBuffer.end() - 1)->boundingBoxes, (dataBuffer.end() - 1)->lidarPoints, shrinkFactor, P_rect_00, R_rect_00, RT);
 
         // Visualize 3D objects
         bVis = true;
-        if(bVis)
+        if (bVis)
         {
-            show3DObjects((dataBuffer.end()-1)->boundingBoxes, cv::Size(4.0, 20.0), cv::Size(2000, 2000), true);
+            show3DObjects((dataBuffer.end() - 1)->boundingBoxes, cv::Size(4.0, 20.0), cv::Size(2000, 2000), true);
         }
         bVis = false;
 
         cout << "#4 : CLUSTER LIDAR POINT CLOUD done" << endl;
-        
-        
+
         // REMOVE THIS LINE BEFORE PROCEEDING WITH THE FINAL PROJECT
         continue; // skips directly to the next image without processing what comes beneath
 
@@ -146,7 +175,7 @@ int main(int argc, const char *argv[])
 
         // convert current image to grayscale
         cv::Mat imgGray;
-        cv::cvtColor((dataBuffer.end()-1)->cameraImg, imgGray, cv::COLOR_BGR2GRAY);
+        cv::cvtColor((dataBuffer.end() - 1)->cameraImg, imgGray, cv::COLOR_BGR2GRAY);
 
         // extract 2D keypoints from current image
         vector<cv::KeyPoint> keypoints; // create empty feature list for current image
@@ -158,7 +187,15 @@ int main(int argc, const char *argv[])
         }
         else
         {
-            //...
+
+            if (detectorType.compare("HARRIS") == 0)
+            {
+                detKeypointsHarris(keypoints, imgGray, false);
+            }
+            else
+            {
+                detKeypointsModern(keypoints, imgGray, detectorType, false);
+            }
         }
 
         // optional : limit number of keypoints (helpful for debugging and learning)
@@ -180,7 +217,6 @@ int main(int argc, const char *argv[])
 
         cout << "#5 : DETECT KEYPOINTS done" << endl;
 
-
         /* EXTRACT KEYPOINT DESCRIPTORS */
 
         cv::Mat descriptors;
@@ -191,7 +227,6 @@ int main(int argc, const char *argv[])
         (dataBuffer.end() - 1)->descriptors = descriptors;
 
         cout << "#6 : EXTRACT DESCRIPTORS done" << endl;
-
 
         if (dataBuffer.size() > 1) // wait until at least two images have been processed
         {
@@ -212,20 +247,18 @@ int main(int argc, const char *argv[])
 
             cout << "#7 : MATCH KEYPOINT DESCRIPTORS done" << endl;
 
-            
             /* TRACK 3D OBJECT BOUNDING BOXES */
 
             //// STUDENT ASSIGNMENT
             //// TASK FP.1 -> match list of 3D objects (vector<BoundingBox>) between current and previous frame (implement ->matchBoundingBoxes)
             map<int, int> bbBestMatches;
-            matchBoundingBoxes(matches, bbBestMatches, *(dataBuffer.end()-2), *(dataBuffer.end()-1)); // associate bounding boxes between current and previous frame using keypoint matches
+            matchBoundingBoxes(matches, bbBestMatches, *(dataBuffer.end() - 2), *(dataBuffer.end() - 1)); // associate bounding boxes between current and previous frame using keypoint matches
             //// EOF STUDENT ASSIGNMENT
 
             // store matches in current data frame
-            (dataBuffer.end()-1)->bbMatches = bbBestMatches;
+            (dataBuffer.end() - 1)->bbMatches = bbBestMatches;
 
             cout << "#8 : TRACK 3D OBJECT BOUNDING BOXES done" << endl;
-
 
             /* COMPUTE TTC ON OBJECT IN FRONT */
 
@@ -251,11 +284,11 @@ int main(int argc, const char *argv[])
                 }
 
                 // compute TTC for current match
-                if( currBB->lidarPoints.size()>0 && prevBB->lidarPoints.size()>0 ) // only compute TTC if we have Lidar points
+                if (currBB->lidarPoints.size() > 0 && prevBB->lidarPoints.size() > 0) // only compute TTC if we have Lidar points
                 {
                     //// STUDENT ASSIGNMENT
                     //// TASK FP.2 -> compute time-to-collision based on Lidar data (implement -> computeTTCLidar)
-                    double ttcLidar; 
+                    double ttcLidar;
                     computeTTCLidar(prevBB->lidarPoints, currBB->lidarPoints, sensorFrameRate, ttcLidar);
                     //// EOF STUDENT ASSIGNMENT
 
@@ -263,7 +296,7 @@ int main(int argc, const char *argv[])
                     //// TASK FP.3 -> assign enclosed keypoint matches to bounding box (implement -> clusterKptMatchesWithROI)
                     //// TASK FP.4 -> compute time-to-collision based on camera (implement -> computeTTCCamera)
                     double ttcCamera;
-                    clusterKptMatchesWithROI(*currBB, (dataBuffer.end() - 2)->keypoints, (dataBuffer.end() - 1)->keypoints, (dataBuffer.end() - 1)->kptMatches);                    
+                    clusterKptMatchesWithROI(*currBB, (dataBuffer.end() - 2)->keypoints, (dataBuffer.end() - 1)->keypoints, (dataBuffer.end() - 1)->kptMatches);
                     computeTTCCamera((dataBuffer.end() - 2)->keypoints, (dataBuffer.end() - 1)->keypoints, currBB->kptMatches, sensorFrameRate, ttcCamera);
                     //// EOF STUDENT ASSIGNMENT
 
@@ -273,10 +306,10 @@ int main(int argc, const char *argv[])
                         cv::Mat visImg = (dataBuffer.end() - 1)->cameraImg.clone();
                         showLidarImgOverlay(visImg, currBB->lidarPoints, P_rect_00, R_rect_00, RT, &visImg);
                         cv::rectangle(visImg, cv::Point(currBB->roi.x, currBB->roi.y), cv::Point(currBB->roi.x + currBB->roi.width, currBB->roi.y + currBB->roi.height), cv::Scalar(0, 255, 0), 2);
-                        
+
                         char str[200];
                         sprintf(str, "TTC Lidar : %f s, TTC Camera : %f s", ttcLidar, ttcCamera);
-                        putText(visImg, str, cv::Point2f(80, 50), cv::FONT_HERSHEY_PLAIN, 2, cv::Scalar(0,0,255));
+                        putText(visImg, str, cv::Point2f(80, 50), cv::FONT_HERSHEY_PLAIN, 2, cv::Scalar(0, 0, 255));
 
                         string windowName = "Final Results : TTC";
                         cv::namedWindow(windowName, 4);
@@ -287,8 +320,7 @@ int main(int argc, const char *argv[])
                     bVis = false;
 
                 } // eof TTC computation
-            } // eof loop over all BB matches            
-
+            }     // eof loop over all BB matches
         }
 
     } // eof loop over all images
--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -1,8 +1,93 @@
-
 #include <numeric>
 #include "matching2D.hpp"
+#include <opencv2/features2d.hpp>
+
+// #include <iostream>
+// #include <fstream>
+// #include <string>
+
+//#include "helper.h"
 
 using namespace std;
+// using cv::DMatch;
+// using cv::BFMatcher;
+// using cv::DrawMatchesFlags;
+// using cv::Feature2D;
+// using cv::ORB;
+// using cv::BRISK;
+// using cv::AKAZE;
+// using cv::FREAK;
+
+// Descriptors & Dectectors
+enum Dectector
+{
+    FAST,
+    BRISK,
+    ORB,
+    AKAZE,
+    SIFT,
+    HARRIS,
+    BRIEF,
+    FREAK
+};
+
+// reference to descriptor code
+// https://github.com/oreillymedia/Learning-OpenCV-3_examples/blob/master/example_16-02.cpp
+
+// void write_a(string filename, string str)
+// {
+
+//     FILE *fp;
+
+//     fp = fopen(filename.c_str(), "a");
+//     if (fp == NULL)
+//     {
+//         perror("Error");
+//         exit(1);
+//     }
+//     else
+//     {
+
+//         fprintf(fp, "%s", str.c_str());
+//     }
+
+//     fclose(fp);
+// }
+
+int stringToIndex(string str)
+{
+
+    if (str.compare("FAST") == 0)
+    {
+        return FAST;
+    }
+    else if (str.compare("BRISK") == 0)
+    {
+        return BRISK;
+    }
+    else if (str.compare("ORB") == 0)
+    {
+        return ORB;
+    }
+    else if (str.compare("AKAZE") == 0)
+    {
+        return AKAZE;
+    }
+    else if (str.compare("SIFT") == 0)
+    {
+        return SIFT;
+    }
+    else if (str.compare("BRIEF") == 0)
+    {
+        return BRIEF;
+    }
+    else if (str.compare("FREAK") == 0)
+    {
+        return FREAK;
+    }
+
+    return -1;
+}
 
 // Find best matches for keypoints in two camera images based on several matching methods
 void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::KeyPoint> &kPtsRef, cv::Mat &descSource, cv::Mat &descRef,
@@ -19,7 +104,7 @@ void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::Key
     }
     else if (matcherType.compare("MAT_FLANN") == 0)
     {
-        // ...
+        matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED);
     }
 
     // perform matching task
@@ -30,9 +115,29 @@ void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::Key
     }
     else if (selectorType.compare("SEL_KNN") == 0)
     { // k nearest neighbors (k=2)
+        uint k = 2;
+        vector<vector<cv::DMatch>> knnMatches;
+        matcher->knnMatch(descSource, descRef, knnMatches, k);
+        float ratio = 0.8;
 
-        // ...
+        for (vector<cv::DMatch> m : knnMatches)
+        {
+            if (m[0].distance < (m[1].distance * ratio))
+            {
+                matches.push_back(m[0]);
+               
+            }
+        }
+
+         cout<< "*** Neighbourhood *** " << matches.size() << endl;
     }
+
+    cout << "# keypoints: " << matches.size() << endl;
+    // write_a("results.csv", "# keypoints: ");
+    // write_a("results.csv", ",");
+    //write_a("results.csv", to_string(matches.size()));
+    //write_a("results.csv", "\n");
+
 }
 
 // Use one of several types of state-of-art descriptors to uniquely identify keypoints
@@ -40,19 +145,63 @@ void descKeypoints(vector<cv::KeyPoint> &keypoints, cv::Mat &img, cv::Mat &descr
 {
     // select appropriate descriptor
     cv::Ptr<cv::DescriptorExtractor> extractor;
-    if (descriptorType.compare("BRISK") == 0)
-    {
+    // if (descriptorType.compare("BRISK") == 0)
+    // {
 
+    //     int threshold = 30;        // FAST/AGAST detection threshold score.
+    //     int octaves = 3;           // detection octaves (use 0 to do single scale)
+    //     float patternScale = 1.0f; // apply this scale to the pattern used for sampling the neighbourhood of a keypoint.
+
+    //     extractor = cv::BRISK::create(threshold, octaves, patternScale);
+    // }
+    // else
+    // {
+
+    //     //...
+    // }
+
+    switch (stringToIndex(descriptorType))
+    {
+    case BRISK:
+    {
         int threshold = 30;        // FAST/AGAST detection threshold score.
         int octaves = 3;           // detection octaves (use 0 to do single scale)
         float patternScale = 1.0f; // apply this scale to the pattern used for sampling the neighbourhood of a keypoint.
 
         extractor = cv::BRISK::create(threshold, octaves, patternScale);
-    }
-    else
-    {
 
-        //...
+        break;
+    }
+
+    case BRIEF:
+    {
+        extractor = cv::xfeatures2d::BriefDescriptorExtractor::create();
+        break;
+    }
+
+    case ORB:
+    {
+        extractor = cv::ORB::create();
+        break;
+    }
+
+    case FREAK:
+    {
+        extractor = cv::xfeatures2d::FREAK::create();
+        break;
+    }
+
+    case SIFT:
+    {
+        extractor = cv::xfeatures2d::SIFT::create();
+        break;
+    }
+
+    case AKAZE:
+    {
+        extractor = cv::AKAZE::create();
+        break;
+    }
     }
 
     // perform feature description
@@ -60,6 +209,12 @@ void descKeypoints(vector<cv::KeyPoint> &keypoints, cv::Mat &img, cv::Mat &descr
     extractor->compute(img, keypoints, descriptors);
     t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
     cout << descriptorType << " descriptor extraction in " << 1000 * t / 1.0 << " ms" << endl;
+    //writeCSV("results.csv", descriptorType);
+    //write_file("results.csv", descriptorType);
+    // write_a("results.csv", "descriptor extraction");
+    // write_a("results.csv", ",");
+    //write_a("results.csv", to_string(t));
+    //write_a("results.csv", ",");
 }
 
 // Detect keypoints in image using the traditional Shi-Thomasi detector
@@ -90,7 +245,12 @@ void detKeypointsShiTomasi(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool b
     }
     t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
     cout << "Shi-Tomasi detection with n=" << keypoints.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
+    // write_a("results.csv", "detector extraction");
+    // write_a("results.csv", ",");
+    // write_a("results.csv", to_string(t));
+    // write_a("results.csv", ",");
 
+    int count = 0;
     // visualize results
     if (bVis)
     {
@@ -99,6 +259,171 @@ void detKeypointsShiTomasi(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool b
         string windowName = "Shi-Tomasi Corner Detector Results";
         cv::namedWindow(windowName, 6);
         imshow(windowName, visImage);
+        imwrite("test_"+ to_string(count++) + ".bmp", visImage);
+        cv::waitKey(0);
+    }
+}
+
+void detKeypointsModern(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, std::string detectorType, bool bVis)
+{
+    //cout << std::stoi( detectorType ) << endl;
+
+    // std::string str = "Hello World";
+    // std::hash<std::string> hasher;
+    // auto hashed = hasher(detectorType); //returns std::size_t
+    // //std::cout << hashed << '\n';
+    //std::cout << typeof(hashed) << endl;
+    //hashed = hasher("FAST");
+    //std::cout << hashed << '\n';
+
+    string windowName;
+    // dummy intialize
+    cv::Ptr<cv::FeatureDetector> detector = cv::BRISK::create();
+    switch (stringToIndex(detectorType))
+    {
+
+    case FAST:
+    {
+
+        std::cout << "Fast selected" << std::endl;
+        int threshold = 30;
+        bool bNMS = true;
+        cv::FastFeatureDetector::DetectorType type = cv::FastFeatureDetector::TYPE_9_16;
+        cv::Ptr<cv::FeatureDetector> detector = cv::FastFeatureDetector::create(threshold, bNMS, type);
+        break;
+    }
+
+    case BRISK:
+
+    {
+        std::cout << "BRISK selected" << std::endl;
+        cv::Ptr<cv::FeatureDetector> detector = cv::BRISK::create();
+        //detector->detect(img, keypoints);
+        break;
+    }
+    case ORB:
+
+    {
+        std::cout << "ORB selected" << std::endl;
+        cv::Ptr<cv::FeatureDetector> detector = cv::ORB::create();
+        //detector->detect(img, keypoints);
+        break;
+    }
+
+    case AKAZE:
+
+    {
+        std::cout << "AKAZE selected" << std::endl;
+        cv::Ptr<cv::FeatureDetector> detector = cv::AKAZE::create();
+        //detector->detect(img, keypoints);
+        break;
+    }
+
+    case SIFT:
+
+    {
+        std::cout << "SIFT selected" << std::endl;
+        cv::Ptr<cv::FeatureDetector> detector = cv::xfeatures2d::SIFT::create();
+        //detector->detect(img, keypoints);
+        break;
+    }
+    };
+
+    double t = (double)cv::getTickCount();
+    detector->detect(img, keypoints);
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    cout << detectorType << " detection with n: " << keypoints.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
+    // write_a("results.csv", "detector extraction");
+    // write_a("results.csv", ",");
+    // write_a("results.csv", to_string(t));
+    // write_a("results.csv", ",");
+
+windowName = detectorType;
+int count = 0;
+if (bVis)
+{
+    cv::Mat visImage = img.clone();
+    cv::drawKeypoints(img, keypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    cv::namedWindow(windowName, 6);
+    imshow(windowName, visImage);
+    imwrite("detect_"+ to_string(count++) + ".bmp", visImage);
+    cv::waitKey(0);
+}
+}
+
+void detKeypointsHarris(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis)
+{
+    // Detector parameters
+    int blockSize = 2;     // for every pixel, a blockSize × blockSize neighborhood is considered
+    int apertureSize = 3;  // aperture parameter for Sobel operator (must be odd)
+    int minResponse = 100; // minimum value for a corner in the 8bit scaled response matrix
+    double k = 0.04;       // Harris parameter (see equation for details)
+
+    // Detect Harris corners and normalize output
+    cv::Mat dst, dst_norm, dst_norm_scaled;
+    dst = cv::Mat::zeros(img.size(), CV_32FC1);
+    double t = (double)cv::getTickCount();
+    cv::cornerHarris(img, dst, blockSize, apertureSize, k, cv::BORDER_DEFAULT);
+    cv::normalize(dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
+    cv::convertScaleAbs(dst_norm, dst_norm_scaled);
+
+    // STUDENTS NEET TO ENTER THIS CODE (C3.2 Atom 4)
+
+    double maxOverlap = 0.0; // max. permissible overlap between two features in %, used during non-maxima suppression
+    for (size_t j = 0; j < dst_norm.rows; j++)
+    {
+        for (size_t i = 0; i < dst_norm.cols; i++)
+        {
+            int response = (int)dst_norm.at<float>(j, i);
+            if (response > minResponse)
+            { // only store points above a threshold
+
+                cv::KeyPoint newKeyPoint;
+                newKeyPoint.pt = cv::Point2f(i, j);
+                newKeyPoint.size = 2 * apertureSize;
+                newKeyPoint.response = response;
+
+                // perform non-maximum suppression (NMS) in local neighbourhood around new key point
+                bool bOverlap = false;
+                for (auto it = keypoints.begin(); it != keypoints.end(); ++it)
+                {
+                    double kptOverlap = cv::KeyPoint::overlap(newKeyPoint, *it);
+                    if (kptOverlap > maxOverlap)
+                    {
+                        bOverlap = true;
+                        if (newKeyPoint.response > (*it).response)
+                        {                      // if overlap is >t AND response is higher for new kpt
+                            *it = newKeyPoint; // replace old key point with new one
+                            break;             // quit loop over keypoints
+                        }
+                    }
+                }
+                if (!bOverlap)
+                {                                     // only add new key point if no overlap has been found in previous NMS
+                    keypoints.push_back(newKeyPoint); // store new keypoint in dynamic list
+                }
+            }
+        } // eof loop over cols
+    }     // eof loop over rows
+
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    cout << "Harris detection with n: " << keypoints.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
+    // write_a("results.csv", "detector extraction H");
+    // write_a("results.csv", ",");
+    // write_a("results.csv", to_string(t));
+    // write_a("results.csv", ",");
+
+
+    int count = 0;
+    // visualize results
+    if (bVis)
+    {
+        cv::Mat visImage = img.clone();
+        cv::drawKeypoints(img, keypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        string windowName = "Harris Corner Detector Results";
+        cv::namedWindow(windowName, 6);
+        imshow(windowName, visImage);
+        imwrite("harris"+ to_string(count++) + ".bmp", visImage);
         cv::waitKey(0);
     }
 }
